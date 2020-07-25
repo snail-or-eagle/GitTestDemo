62fdc9b3d583c0ab38f404ff529d2571bc5728b2
loulamli_e <1215246272@qq.com>
2020-07-25 09:34:16 +0800

    commit on branch hot_fix

--- src/main/java/com/itguigu/git/Demo01.java
+++ src/main/java/com/itguigu/git/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("I love huangxiaolan");
 		System.out.println("I love zhangjiayi");
+		System.out.println("我要娶张嘉仪做老婆！");
 	}
 }
